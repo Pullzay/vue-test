ff68d980ce3d41363f779c302a68083147a58ee3
VΛndreev <vandreev@web-canape.com>
2021-06-29 08:25:23 +0300

    Сортировка по категориям

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -7,8 +7,13 @@
       <div class="catalog-cart">Cart {{ CART.length }}</div>
     </router-link>
     <div v-else class="catalog-cart">Cart 0</div>
+    <Select
+      :options="categories"
+      @select="sortByCategoris"
+      :selected="selected"
+    />
     <CatalogItem
-      v-for="product in PRODUCTS"
+      v-for="product in filteredProducts"
       :key="product.article"
       :product_data="product"
       @addToCart="addToCart"
@@ -18,20 +23,39 @@
 
 <script>
 import CatalogItem from "./CatalogItem.vue";
+import Select from "./Select.vue";
 import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "Catalog",
   components: {
     CatalogItem,
+    Select,
   },
   computed: {
     ...mapGetters(["PRODUCTS", "CART"]),
+    filteredProducts() {
+      if (this.sortedProducts.length) {
+        return this.sortedProducts;
+      } else {
+        return this.PRODUCTS;
+      }
+    },
   },
   methods: {
     ...mapActions(["GET_PRODUCTS_FROM_API", "ADD_TO_CART"]),
     addToCart(data) {
       this.ADD_TO_CART(data);
+    },
+    sortByCategoris(category) {
+      this.sortedProducts = [];
+      let vm = this;
+      this.PRODUCTS.map((item) => {
+        if (item.category === category.name) {
+          vm.sortedProducts.push(item);
+        }
+      });
+      this.selected = category.name;
     },
   },
   mounted() {
@@ -42,7 +66,15 @@ export default {
     });
   },
   data() {
-    return {};
+    return {
+      categories: [
+        { name: "Все", value: "all" },
+        { name: "Мужские", value: "man" },
+        { name: "Женские", value: "woman" },
+      ],
+      selected: "Все",
+      sortedProducts: [],
+    };
   },
 };
 </script>
--- src/components/MainWrapper.vue
+++ src/components/MainWrapper.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="main-wrapper">
-    <Select :options="options" @select="optionSelect" :selected="selected" />
     <keep-alive>
       <router-view> </router-view>
     </keep-alive>
@@ -8,29 +7,7 @@
 </template>
 
 <script>
-import Select from "./Select.vue";
-
 export default {
-  components: {
-    Select,
-  },
   name: "MainWrapper",
-  data() {
-    return {
-      options: [
-        { name: "Option 1", value: 1 },
-        { name: "Option 2", value: 2 },
-        { name: "Option 3", value: 3 },
-        { name: "Option 4", value: 4 },
-        { name: "Option 5", value: 5 },
-      ],
-      selected: "Select",
-    };
-  },
-  methods: {
-    optionSelect(option) {
-      this.selected = option.name;
-    },
-  },
 };
 </script>
