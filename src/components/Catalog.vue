7539f147e6bded1678175cfc1ceea44ebefb0cb2
VΛndreev <vandreev@web-canape.com>
2021-06-29 08:57:07 +0300

    Сортировка по цене ползунком

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -7,11 +7,35 @@
       <div class="catalog-cart">Cart {{ CART.length }}</div>
     </router-link>
     <div v-else class="catalog-cart">Cart 0</div>
-    <Select
-      :options="categories"
-      @select="sortByCategoris"
-      :selected="selected"
-    />
+    <div class="filters">
+      <Select
+        :options="categories"
+        @select="sortByCategories"
+        :selected="selected"
+      />
+      <div class="range-slider">
+        <input
+          type="range"
+          min="0"
+          max="9000"
+          step="10"
+          v-model.number="minPrice"
+          @change="setRangeSlider"
+        />
+        <input
+          type="range"
+          min="0"
+          max="9000"
+          step="10"
+          v-model.number="maxPrice"
+          @change="setRangeSlider"
+        />
+      </div>
+      <div class="range-values">
+        <p>Min: {{ minPrice }}</p>
+        <p>Max: {{ maxPrice }}</p>
+      </div>
+    </div>
     <CatalogItem
       v-for="product in filteredProducts"
       :key="product.article"
@@ -47,21 +71,33 @@ export default {
     addToCart(data) {
       this.ADD_TO_CART(data);
     },
-    sortByCategoris(category) {
-      this.sortedProducts = [];
+    sortByCategories(category) {
       let vm = this;
-      this.PRODUCTS.map((item) => {
-        if (item.category === category.name) {
-          vm.sortedProducts.push(item);
-        }
+      this.sortedProducts = [...this.PRODUCTS];
+      this.sortedProducts = this.sortedProducts.filter((item) => {
+        return item.price >= vm.minPrice && item.price <= vm.maxPrice;
       });
-      this.selected = category.name;
+      if (category) {
+        this.sortedProducts = this.sortedProducts.filter((e) => {
+          vm.selected = category.name;
+          return e.category === category.name;
+        });
+      }
+    },
+    setRangeSlider() {
+      if (this.minPrice > this.maxPrice) {
+        let tmp = this.maxPrice;
+        this.maxPrice = this.minPrice;
+        this.minPrice = tmp;
+      }
+      this.sortByCategories();
     },
   },
   mounted() {
     this.GET_PRODUCTS_FROM_API().then((responce) => {
       if (responce.data) {
         console.log("Готово");
+        this.sortByCategories();
       }
     });
   },
@@ -74,6 +110,8 @@ export default {
       ],
       selected: "Все",
       sortedProducts: [],
+      minPrice: 0,
+      maxPrice: 9000,
     };
   },
 };
